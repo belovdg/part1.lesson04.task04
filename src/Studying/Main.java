9cb90ff3fad32e28cf394fe607aa3d0abe9880eb
belovdg <belovdg87@yandex.ru>
2020-12-18 14:22:00 +0300

    Добавлено название полей и информативность

--- src/Studying/Main.java
+++ src/Studying/Main.java
@@ -3,12 +3,16 @@ package Studying;
 public class Main {
 
     public static void main(String[] args) {
-        int max=2;
-        int progr=1;
+        int max=6;
+        int progr=3;
+        System.out.println("Максимальное число в прогрессии = " + max);
+        System.out.println("Изначальное кол-во переменных в прогрессии = " + progr);
+        System.out.print("Вывод арифметической прогрессии:" + "\n");
         for(int i=1;i<=max;i++)
         {
             for(int j=0;j<progr;j++)
-                System.out.print(i+" ");
+
+                System.out.print(i + " ");
             progr+=1;
         }
         System.out.println();
